77a53189001009e224a66f5fe35eb7d79282dbd7
Vu Hoang Trung - Frontend <hoangtrung@mobcec.com>
2023-11-29 08:41:44 +0700

    feat: initial Source NextJS 14

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  images: {
+    loader: 'custom',
+    loaderFile: '/src/lib/utils/loader.ts'
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'd1ubwt7z1ubyyw.cloudfront.net'
+    //   }
+    // ]
+  }
+}
 
 module.exports = nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,58 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
+
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1232px'
       },
+      padding: {
+        DEFAULT: '16px'
+      }
+    },
+    extend: {
+      fontSize: {
+        10: '10px',
+        12: '12px',
+        14: '14px',
+        16: '16px',
+        18: '18px',
+        20: '20px',
+        22: '22px',
+        24: '24px',
+        26: '26px',
+        28: '28px',
+        30: '30px',
+        32: '32px',
+        34: '34px',
+        36: '36px',
+        38: '38px',
+        40: '40px',
+        44: '44px',
+        48: '48px',
+        64: '64px'
+      },
+      boxShadow: {
+        primary: '0px 2px 8px 0px rgba(0, 0, 0, 0.10)',
+        secondary: '0px 4px 16px 0px rgba(0, 0, 0, 0.20)'
+      },
+      lineHeight: {
+        '1-1': '1.1',
+        '1-2': '1.2',
+        '1-4': '1.4',
+      }
     },
   },
   plugins: [],
 }
+
 export default config
